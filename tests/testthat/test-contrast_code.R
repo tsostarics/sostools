1136949e14982a85ec5a5fe0329c42621890021e
Thomas Sostarics <tsostarics@gmail.com>
2021-05-09 15:42:43 -0500

    remove extra copied test

--- tests/testthat/test-contrast_code.R
+++ tests/testthat/test-contrast_code.R
@@ -60,33 +60,6 @@ test_that("four level helmert coding works", {
                ignore_attr = TRUE)
 })
 
-test_that("four level helmert coding works", {
-  tst_data <-
-    tibble::tribble(
-      ~two, ~three, ~four,
-      "a",    "a",   "a",
-      "b",    "b",   "b",
-      "a",    "c",   "c",
-      "b",    "a",   "d"
-    ) %>%
-    dplyr::mutate(dplyr::across(tidyselect::everything(), factor))
-
-  expect_equal(colnames(contrast_code(tst_data$four, matrix(c(-1/4, -1/4, -1/4, 3/4,
-                                                              -1/3, -1/3, 2/3, 0,
-                                                              -1/2, 1/2, 0, 0),
-                                                            nrow = 4))),
-               c("d","c","b"))
-  expect_equal(contrast_code(tst_data$four, matrix(c(-1/4, -1/4, -1/4, 3/4,
-                                                     -1/3, -1/3, 2/3, 0,
-                                                     -1/2, 1/2, 0, 0),
-                                                   nrow = 4)),
-               matrix(c(-1/4, -1/4, -1/4, 3/4,
-                        -1/3, -1/3, 2/3, 0,
-                        -1/2, 1/2, 0, 0),
-                      nrow = 4),
-               ignore_attr = TRUE)
-})
-
 test_that("polynomial coding works", {
   tst_data <-
     tibble::tribble(
