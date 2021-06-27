771570a48b97e9ddaad31b7ab0ff0b2892841e6c
Thomas Sostarics <tsostarics@gmail.com>
2021-06-27 16:04:08 -0500

    add manually specified comparison name handling

--- R/manual_code.R
+++ R/manual_code.R
@@ -14,7 +14,20 @@ manual_code <- function(factor_col, coding_matrix = NA) {
     stop("This factor has more than 2 levels, please provide a matrix.")
   }
 
+  # given_levels <- rownames(coding_matrix)
+  given_comparisons <- colnames(coding_matrix)
   labels <- dimnames(contrasts(factor_col))
+
+  # if (!is.null(given_levels) & !all(rownames(coding_matrix) %in% labels[[1]]))
+  #   warning("Factor levels in matrix not the same as data levels, order may not be as intended")
+
+  # Handling if the given matrix has prespecified comparison labels
+  # if (!is.null(given_levels))
+  #   labels[[1]] <- given_levels
+
+  if (!is.null(given_comparisons))
+    labels[[2]] <- given_comparisons
+
   # Default behavior for 2 level factors
   if (!any(is.na(coding_matrix))) {
     new_contrasts <- coding_matrix
@@ -23,6 +36,8 @@ manual_code <- function(factor_col, coding_matrix = NA) {
   }
 
   dimnames(new_contrasts) <- labels
+  if (!is.null(given_comparisons))
+    return(new_contrasts)
   .reset_comparison_labels(new_contrasts)
 }
 
@@ -34,10 +49,11 @@ manual_code <- function(factor_col, coding_matrix = NA) {
       colnames(contr_mat) <- paste(rownames(contr_mat)[-1], rownames(contr_mat)[-n_levels], sep = "-")
   else if (n_levels != 2 & .check_forward_difference(contr_mat))
       colnames(contr_mat) <- paste(rownames(contr_mat)[-n_levels], rownames(contr_mat)[-1], sep = "-")
-  else
-    colnames(contr_mat) <-
-      unname(apply(contr_mat, 2, \(x) rownames(contr_mat)[x > 0]))
-
+  else {
+    new_colnames <- unname(apply(contr_mat, 2, \(x) rownames(contr_mat)[x > 0]))
+    if (all(vapply(new_colnames, \(x) length(x) == 1, TRUE)))
+      colnames(contr_mat) <- new_colnames
+  }
   contr_mat
 }
 
