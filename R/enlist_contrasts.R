2b53a46c7280a86bf58b4a4647e244e0d6ed4667
Thomas Sostarics <tsostarics@gmail.com>
2021-06-29 13:51:01 -0500

    change handling of reference level to be parsed instead of quoted

--- R/enlist_contrasts.R
+++ R/enlist_contrasts.R
@@ -75,11 +75,14 @@ enlist_contrasts <- function(model_data, ...) {
 #' @return A contrast matrix
 .process_contrasts <- function(model_data, char_formula) {
   coding_scheme <- strsplit(char_formula[[3L]], " + ", fixed = TRUE)[[1L]]
+  reference_level <- NA
+  if (length(coding_scheme) == 2)
+    reference_level <- eval(parse(text = coding_scheme[[2L]]))
 
   contrast_code(
     factor_col = model_data[[char_formula[[2L]]]],
     code_by = eval(parse(text = coding_scheme[[1L]])),
-    reference_level = ifelse(length(coding_scheme) == 2, coding_scheme[[2]], NA)
+    reference_level = reference_level
   )
 
 }
