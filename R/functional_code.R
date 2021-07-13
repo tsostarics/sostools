9baa6532fd79a7a2a9add2e5ed7f5bcc631b5d3e
Thomas Sostarics <tsostarics@gmail.com>
2021-07-13 18:11:59 -0500

    add set intercept argument

--- R/functional_code.R
+++ R/functional_code.R
@@ -4,10 +4,13 @@
 #' @param coding_fx A function that generates contrast matrices
 #' @param reference_level Label of the desired reference level, will default to
 #' the first label in dimnames of the original contrasts
+#' @param set_intercept Label of the desired intercept level, will default to
+#' scheme's default (typically grand mean, unless using contr.treatment, which does
+#' the reference level)
 #'
 #' @export
 #' @importFrom stats contr.helmert contr.poly contr.treatment
-functional_code <- function(factor_col, coding_fx, reference_level=NA) {
+functional_code <- function(factor_col, coding_fx, reference_level=NA, set_intercept = NA) {
   labels <- dimnames(contrasts(factor_col))
   reference_i <- NA
   if (is.na(reference_level))
@@ -31,7 +34,36 @@ functional_code <- function(factor_col, coding_fx, reference_level=NA) {
                                            reference_i)
 
   dimnames(new_contrasts) <- labels
-  .reset_comparison_labels(new_contrasts)
+  new_contrasts <- .reset_comparison_labels(new_contrasts)
+
+  if (!is.na(set_intercept))
+    return(.set_intercept(new_contrasts, set_intercept))
+
+  return(new_contrasts)
+}
+
+.set_intercept <- function(contrast_matrix, intercept_level) {
+  if (!intercept_level %in% rownames(contrast_matrix))
+    stop("Specified level to use as intercept not found in factor level names")
+
+  n_levels <- nrow(contrast_matrix)
+  # Add back the missing intercept, solve the transpose for hypothesis matrix
+  hypothesis_matrix <-
+    c(rep(1,n_levels), contrast_matrix) |>
+    matrix(nrow = n_levels) |>
+    t() |>
+    solve()
+
+  intercept_column <- rep(0, n_levels)
+  intercept_index <- which(rownames(contrast_matrix) == intercept_level)
+  intercept_column[intercept_index] <- 1
+  hypothesis_matrix[,1] <- intercept_column
+
+  # Resolve the new hypothesis matrix and remove intercept column for contrasts
+  new_contrasts <- solve(t(hypothesis_matrix))
+  new_contrasts <- new_contrasts[,seq_len(n_levels)[-1L]]
+  dimnames(new_contrasts) <- dimnames(contrast_matrix)
+  new_contrasts
 }
 
 .check_non_references <- function(coding_fx) {
