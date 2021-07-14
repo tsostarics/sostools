aa65850b6b374cd8639a776effe7c999f6cf4791
Thomas Sostarics <tsostarics@gmail.com>
2021-07-14 14:17:01 -0500

    fix bug where contr.treatment reference level wouldnt change

--- R/switch_reference_level.R
+++ R/switch_reference_level.R
@@ -10,10 +10,10 @@
 #' @return A matrix with the correct values for the reference level
 #' @export
 .switch_reference_level <- function(contrast_matrix, coding_fx, old_reference, new_reference){
-  if (is.na(new_reference) | new_reference == old_reference)
-    return(contrast_matrix)
   if (identical(coding_fx, contr.treatment))
     old_reference <- 1L
+  if (is.na(new_reference) | new_reference == old_reference)
+    return(contrast_matrix)
   reference_row <- as.matrix(contrast_matrix[old_reference,]) # as.matrix needed for n_levels = 2
   contrast_matrix[old_reference,] <- as.matrix(contrast_matrix[new_reference,])
   contrast_matrix[new_reference,] <- reference_row
