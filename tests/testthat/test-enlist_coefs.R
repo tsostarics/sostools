78c8b54491defed7d843290ded7585da4ba03a79
Thomas Sostarics <tsostarics@gmail.com>
2021-07-29 09:10:01 -0500

    update test

--- tests/testthat/test-enlist_coefs.R
+++ tests/testthat/test-enlist_coefs.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("enlist coefficients works", {
+  tstmdl <- lm(mpg ~ cyl*gear, data = set_contrasts(mtcars, gear ~ scaled_sum_code))
+  coefs <- enlist_coefs(tstmdl)
+  expect_equal(as.character(coefs$Intercept), r"($(\beta = 37.21, t = 12.55, s.e. = 2.96, p < .001)$)")
+  expect_equal(as.character(coefs$`cyl:gear5`), r"($(\beta = -1.37, t = -1.25, s.e. = 1.1, p=0.223)$)")
+})
+
+test_that("p value adjustment works", {
+  tstmdl <- lm(mpg ~ cyl*gear, data = set_contrasts(mtcars, gear ~ scaled_sum_code))
+  coefs <- enlist_coefs(tstmdl, correct = "gear")
+  expect_equal(as.character(coefs$Intercept), r"($(\beta = 37.21, t = 12.55, s.e. = 2.96, p < .001)$)")
+  expect_equal(as.character(coefs$`cyl:gear5`), r"($(\beta = -1.37, t = -1.25, s.e. = 1.1, p=0.508)$)")
 })
