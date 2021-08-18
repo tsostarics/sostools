3bcee71e8af5bfecf513ade7a19060e9c0037c9c
Thomas Sostarics <tsostarics@gmail.com>
2021-08-18 15:26:06 -0500

    add additional tests

---
+++ tests/testthat/test-parse_formula.R
@@ -0,0 +1,78 @@
+test_that("Formula validation works", {
+  f_onesided <- ~ gear
+  f_biglhs1 <- gear + 2 ~ contr.poly
+  f_biglhs2 <- 2 + gear ~ contr.poly
+  f_toomanyops <- gear ~ contr.poly + "a" + 2
+  f_badop1 <- gear ~ contr.poly + "a"^2
+  f_badop2 <- gear ~ contr.poly + 2 %in% a
+  f_badseq <- gear ~ contr.poly + 3:5
+  f_firstterm1 <- gear ~ -1 + contr.poly
+  f_firstterm2 <- gear ~ 1 + contr.poly
+
+  expect_error(.check_if_valid_formula(f_onesided, deparse(f_onesided), deparse(f_onesided)),
+               regexp = "two sided")
+  expect_error(.check_if_valid_formula(f_biglhs1, deparse(f_biglhs1), deparse(f_biglhs1)),
+               regexp = "1 variable name on left")
+  expect_error(.check_if_valid_formula(f_biglhs2, deparse(f_biglhs2), deparse(f_biglhs2)),
+               regexp = "1 variable name on left")
+  expect_error(.check_if_valid_formula(f_toomanyops, deparse(f_toomanyops), deparse(f_toomanyops)),
+               regexp = "may only use \\+, \\*, and - once")
+  expect_error(.check_if_valid_formula(f_badop1, deparse(f_badop1), deparse(f_badop1)),
+               regexp = "operators in this formula")
+  expect_error(.check_if_valid_formula(f_badop2, deparse(f_badop2), deparse(f_badop2)),
+               regexp = "operators in this formula")
+  expect_error(.check_if_valid_formula(f_badseq, deparse(f_badseq), deparse(f_badseq)),
+               regexp = "may only be used to drop trends with the - operator")
+  expect_error(.check_if_valid_formula(f_firstterm1, deparse(f_firstterm1), deparse(f_firstterm1)),
+               regexp = "in right hand side must be a contrast")
+  expect_error(.check_if_valid_formula(f_firstterm2, deparse(f_firstterm2), deparse(f_firstterm2)),
+               regexp = "in right hand side must be a contrast")
+
+})
+
+test_that("Formula validation with matrix calls works", {
+  f_mat1 <- gear ~ matrix(c(0.75, -0.25, -0.25,
+                            -0.25/4, -0.25*2, 0.75^2.3,
+                            -0.25, -0.25, -0.25%in%c(1,2,3),
+                            -0.25, 0.75, -0.25) %>% abs(), nrow = 4) + 4
+  char_formula <- deparse(f_mat1, width.cutoff = 500L)
+  no_matrix_string <- gsub(r"(matrix\((.+\(.+\)?)(, .+)*\) ?)","",char_formula)
+  expect_true(.check_if_valid_formula(f_mat1, char_formula, no_matrix_string))
+})
+
+test_that("Formula parsing with matrix call works", {
+  f_mat1 <- gear ~ matrix(c(0.75, -0.25, -0.25,
+                            -0.25/4, -0.25*2, 0.75^2.3,
+                            -0.25, -0.25, -0.25%in%c(1,2,3),
+                            -0.25, 0.75, -0.25) %>% abs(), nrow = 4) + 4 * 2 -3:5
+  params <- .parse_formula(f_mat1, TRUE)
+
+  reference <-
+    list("factor_col" = "gear",
+         "reference_level" = "4",
+         "intercept_level" = "2",
+         "drop_trends" = "3:5")
+
+  expect_equal(params, reference)
+})
+
+test_that("Formula parsing with functions works", {
+  f_allops <- gear ~ contr.poly + "a" * "b" -a:b
+  params_allops <- .parse_formula(f_allops, FALSE)
+  reference_allops <-
+    list("factor_col" = "gear",
+         "reference_level" = '"a"',
+         "intercept_level" = '"b"',
+         "drop_trends" = "a:b")
+
+  f_noops <- gear ~ contr.poly
+  params_noops <- .parse_formula(f_noops, FALSE)
+  reference_noops <-
+    list("factor_col" = "gear",
+         "reference_level" = NA,
+         "intercept_level" = NA,
+         "drop_trends" = NA)
+
+  expect_equal(params_allops, reference_allops)
+  expect_equal(params_noops, reference_noops)
+  })
--- tests/testthat/test-set_contrasts.R
+++ tests/testthat/test-set_contrasts.R
@@ -1,7 +1,8 @@
 test_that("set contrast equivalent to manual contrast codes", {
   tst_df <- set_contrasts(dplyr::mutate(mtcars, gear = factor(gear)),
                           gear ~ helmert_code,
-                          cyl ~ contr.sum)
+                          cyl ~ contr.sum,
+                          verbose = FALSE)
 
   comparison_df <- dplyr::mutate(mtcars, gear = factor(gear), cyl = factor(cyl))
   contrasts(comparison_df$cyl) <- contrast_code(comparison_df$cyl, contr.sum)
@@ -18,11 +19,14 @@ test_that("set contrast equivalent to manual contrast codes", {
   })
 
 test_that("Ignoring dropped levels in orthogonal polynomial contrasts", {
-
-  expect_warning(aaa <- set_contrasts(mtcars, carb ~ contr.poly - 4:6),
+  expect_warning(set_contrasts(mtcars, carb ~ contr.poly - 4:6, verbose = FALSE),
                  regexp = r"(Cannot drop trends .+ formula\.)")
-  expect_warning(aaa <- set_contrasts(mtcars, carb ~ contr.poly - 4:6, gear ~ contr.poly - 2:3),
+  expect_warning(aaa <- set_contrasts(mtcars, carb ~ contr.poly - 4:6, gear ~ contr.poly - 2:3, verbose = FALSE),
                  regexp = r"(Cannot drop trends .+ formulas\.)")
+  expect_warning(set_contrasts(mtcars, gear ~ contr.sum - 2:3, verbose = FALSE),
+                 regexp = "used in invalid context")
+
+
   expect_equal(contrasts(aaa$carb), contr.poly(6), ignore_attr = TRUE)
   expect_equal(contrasts(aaa$gear), contr.poly(3), ignore_attr = TRUE)
 
