36c102a7016697dd580326f4c8bcbbdc9117e364
Thomas Sostarics <tsostarics@gmail.com>
2021-08-22 13:10:38 -0500

    add wrappers for treatment, sum, and polynomial coding

---
+++ R/polynomial_code.R
@@ -0,0 +1,46 @@
+#' Orthogonal Polynomial code
+#'
+#' Wrapper around R's built in function
+#'
+#' For n levels of factors where k in 1:n, generate a matrix with n-1 comparisons where each
+#' comparison looks for a polynomial trend of degree k where each polynomial
+#' is independent of the others.
+#'
+#' @param n_levels number of levels for this factor
+#'
+#' @return A matrix of orthogonal polynomial contrasts
+#' @export
+orth_polynomial_code <- function(n_levels) {
+  contr.poly(n_levels)
+}
+
+#' Raw Polynomial code
+#'
+#' Make raw polynomial contrast, rather than orthogonal ones. Normally you
+#' would use orthogonal polynomials, so make sure this is what you want. Using
+#' raw polynomials may increase the collinearity in your model, especially with
+#' higher levels.
+#'
+#' For n levels of factors where k in 1:n, generate a matrix with n-1 comparisons where each
+#' comparison looks for a polynomial trend of degree k where each polynomial
+#' may be correlated with the others. Normally you would use orthogonal polynomials.
+#'
+#' @param n_levels number of levels for this factor
+#'
+#' @return A matrix of raw polynomial contrasts
+#' @export
+#'
+#' @importFrom stats poly
+raw_polynomial_code <- function(n_levels) {
+  contrmat <- poly(seq_len(n_levels), n_levels - 1, raw = TRUE)
+  attr(contrmat, "degree") <- NULL
+  contrmat
+}
+
+
+.is_polynomial_scheme <- function(scheme) {
+  # maybe include a check on the numbers to handle raw passed matrices
+  checks <- list(contr.poly, orth_polynomial_code, raw_polynomial_code)
+
+  any(vapply(checks, function(x) identical(x, scheme), FUN.VALUE = TRUE))
+}
--- R/treatment_code.R
+++ R/treatment_code.R
@@ -1,16 +1,15 @@
-#' Sum code
+#' Treatment code
 #'
 #' Wrapper around R's built in function
 #'
 #' For n levels of factors, generate a matrix with n-1 comparisons where:
-#' Reference level = -1
+#' Reference level = 0
 #' Comparison level = 1
-#' All others = 0
 #'
 #' @param n_levels number of levels for this factor
 #'
-#' @return A matrix of sum coded contrasts, unlabeled
+#' @return A matrix of treatment coded contrasts, unlabeled
 #' @export
-sum_code <- function(n_levels) {
-  contr.sum(n_levels)
+treatment_code <- function(n_levels) {
+  contr.treatment(n_levels)
 }
