7e8e16a7c1dd95335b38fd5451c45d59cf978642
Thomas Sostarics <tsostarics@gmail.com>
2021-09-05 08:33:50 -0500

    add test for non = errors

--- tests/testthat/test-formula_to_char.R
+++ tests/testthat/test-formula_to_char.R
@@ -1,3 +1,7 @@
 test_that("Character coercion error message works", {
   expect_error(set_contrasts(mtcars, carb = contr.poly), regexp = "Did you use = instead of ~")
 })
+
+test_that("Character coercion error message works", {
+  expect_error(set_contrasts(mtcars, carb = contr.poly()), regexp = "is missing, with no default")
+})
