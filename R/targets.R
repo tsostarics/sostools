0955601e34a798a0a04dfad4834ddc11e5911250
Thomas Sostarics <tsostarics@gmail.com>
2025-07-05 10:31:27 -0500

    add targets functions

--- R/targets.R
+++ R/targets.R
@@ -69,3 +69,59 @@ tar_load_em <- function() {
   assign(object_name, readRDS(targets::tar_read_raw(object_name)), pos = parent.frame())
 
 }
+
+
+
+#' Get reverse dependencies of a node
+#'
+#' Given the name of a target and the edges dataframe from `targets::tar_network()`
+#' return the downstream nodes that depend on the given target. Will recurse
+#' through the entire DAG.
+#'
+#' @param from Node to search from
+#' @param edges Edges dataframe from tar_network
+#'
+#' @return Character vector of target names
+get_reverse_dependencies <- function(from = "write_constant_scale_files", edges) {
+  # Base case
+  if (!from %in% edges$from)
+    return(from)
+
+  new_nodes <- edges[edges$from == from,][['to']]
+
+  nodes <- lapply(new_nodes, \(x) c(from, get_reverse_dependencies(x, edges)))
+
+  unique(c(nodes, recursive=TRUE))
+
+}
+
+#' Delete and invalidate reverse dependencies
+#'
+#' Targets doesn't play perfectly with praat scripts since the usual workflow
+#' is to create large numbers of files. When these files are recreated, targets
+#' doesn't flag them as being out of date since the files themselves aren't
+#' tracked (too many to do so and also the repository option wouldn't work for
+#' me). To circumvent this, this function will invalidate and delete all of the
+#' downstream target information from a given node, which will force them
+#' to be rerun.
+#'
+#' tar_network() can take a while to run, so it's recommended to save this
+#' to a variable if you think you might need to rerun this a few times as
+#' you try new things in praat.
+#'
+#' @param from Node to delete from
+#' @param network Output of targets::tar_network()
+#'
+#' @return Character vector of nodes that were invalidated and deleted
+flush_targets <- function(from, network = targets::tar_network()) {
+  rlang::is_installed("targets")
+
+  nodes_to_flush <- get_reverse_dependencies(from, network$edges)
+
+  targets::tar_invalidate(all_of(nodes_to_flush))
+  targets::tar_delete(all_of(nodes_to_flush))
+
+  message(glue::glue("Invalidated and deleted {length(nodes_to_flush)} nodes from {from}"))
+
+  nodes_to_flush
+}
